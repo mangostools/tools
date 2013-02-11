54bb3a29a289c1ddc237879552ba32cf97d29ce0
Anthony O'Dell <antz@cix.co.uk>
2013-02-11 01:42:28 +0000

    changed model to generic, now dynamic table build

--- MaNGOSDatabaseTool/MaNGOSDatabaseTool/frmMain.Designer.vb
+++ MaNGOSDatabaseTool/MaNGOSDatabaseTool/frmMain.Designer.vb
@@ -24,27 +24,7 @@ Partial Class frmMain
     Private Sub InitializeComponent()
         Me.btnQuit = New System.Windows.Forms.Button()
         Me.tabMain = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.btnReload = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.btnReloadItems = New System.Windows.Forms.Button()
-        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
-        Me.TabPage3 = New System.Windows.Forms.TabPage()
-        Me.btnReloadGameObjects = New System.Windows.Forms.Button()
-        Me.DataGridView3 = New System.Windows.Forms.DataGridView()
-        Me.TabPage4 = New System.Windows.Forms.TabPage()
-        Me.btnReloadCreatureAi = New System.Windows.Forms.Button()
-        Me.DataGridView4 = New System.Windows.Forms.DataGridView()
-        Me.tabMain.SuspendLayout()
-        Me.TabPage1.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.TabPage2.SuspendLayout()
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.TabPage3.SuspendLayout()
-        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.TabPage4.SuspendLayout()
-        CType(Me.DataGridView4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnQuit
@@ -58,163 +38,35 @@ Partial Class frmMain
         '
         'tabMain
         '
-        Me.tabMain.Controls.Add(Me.TabPage1)
-        Me.tabMain.Controls.Add(Me.TabPage2)
-        Me.tabMain.Controls.Add(Me.TabPage3)
-        Me.tabMain.Controls.Add(Me.TabPage4)
         Me.tabMain.Location = New System.Drawing.Point(12, 29)
         Me.tabMain.Name = "tabMain"
         Me.tabMain.SelectedIndex = 0
         Me.tabMain.Size = New System.Drawing.Size(906, 431)
         Me.tabMain.TabIndex = 3
         '
-        'TabPage1
+        'Button1
         '
-        Me.TabPage1.Controls.Add(Me.btnReload)
-        Me.TabPage1.Controls.Add(Me.DataGridView1)
-        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(898, 405)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "NPC's"
-        Me.TabPage1.UseVisualStyleBackColor = True
-        '
-        'btnReload
-        '
-        Me.btnReload.Location = New System.Drawing.Point(6, 6)
-        Me.btnReload.Name = "btnReload"
-        Me.btnReload.Size = New System.Drawing.Size(75, 23)
-        Me.btnReload.TabIndex = 4
-        Me.btnReload.Text = "Reload"
-        Me.btnReload.UseVisualStyleBackColor = True
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(6, 35)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(886, 364)
-        Me.DataGridView1.TabIndex = 3
-        '
-        'TabPage2
-        '
-        Me.TabPage2.Controls.Add(Me.btnReloadItems)
-        Me.TabPage2.Controls.Add(Me.DataGridView2)
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(898, 405)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Items"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
-        'btnReloadItems
-        '
-        Me.btnReloadItems.Location = New System.Drawing.Point(6, 6)
-        Me.btnReloadItems.Name = "btnReloadItems"
-        Me.btnReloadItems.Size = New System.Drawing.Size(75, 23)
-        Me.btnReloadItems.TabIndex = 4
-        Me.btnReloadItems.Text = "Reload"
-        Me.btnReloadItems.UseVisualStyleBackColor = True
-        '
-        'DataGridView2
-        '
-        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView2.Location = New System.Drawing.Point(6, 35)
-        Me.DataGridView2.Name = "DataGridView2"
-        Me.DataGridView2.Size = New System.Drawing.Size(886, 364)
-        Me.DataGridView2.TabIndex = 3
-        '
-        'TabPage3
-        '
-        Me.TabPage3.Controls.Add(Me.btnReloadGameObjects)
-        Me.TabPage3.Controls.Add(Me.DataGridView3)
-        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage3.Name = "TabPage3"
-        Me.TabPage3.Size = New System.Drawing.Size(898, 405)
-        Me.TabPage3.TabIndex = 2
-        Me.TabPage3.Text = "Game Objects"
-        Me.TabPage3.UseVisualStyleBackColor = True
-        '
-        'btnReloadGameObjects
-        '
-        Me.btnReloadGameObjects.Location = New System.Drawing.Point(6, 6)
-        Me.btnReloadGameObjects.Name = "btnReloadGameObjects"
-        Me.btnReloadGameObjects.Size = New System.Drawing.Size(75, 23)
-        Me.btnReloadGameObjects.TabIndex = 6
-        Me.btnReloadGameObjects.Text = "Reload"
-        Me.btnReloadGameObjects.UseVisualStyleBackColor = True
-        '
-        'DataGridView3
-        '
-        Me.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView3.Location = New System.Drawing.Point(6, 35)
-        Me.DataGridView3.Name = "DataGridView3"
-        Me.DataGridView3.Size = New System.Drawing.Size(886, 364)
-        Me.DataGridView3.TabIndex = 5
-        '
-        'TabPage4
-        '
-        Me.TabPage4.Controls.Add(Me.btnReloadCreatureAi)
-        Me.TabPage4.Controls.Add(Me.DataGridView4)
-        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage4.Name = "TabPage4"
-        Me.TabPage4.Size = New System.Drawing.Size(898, 405)
-        Me.TabPage4.TabIndex = 3
-        Me.TabPage4.Text = "Creature_ai_Scripts"
-        Me.TabPage4.UseVisualStyleBackColor = True
-        '
-        'btnReloadCreatureAi
-        '
-        Me.btnReloadCreatureAi.Location = New System.Drawing.Point(6, 6)
-        Me.btnReloadCreatureAi.Name = "btnReloadCreatureAi"
-        Me.btnReloadCreatureAi.Size = New System.Drawing.Size(75, 23)
-        Me.btnReloadCreatureAi.TabIndex = 6
-        Me.btnReloadCreatureAi.Text = "Reload"
-        Me.btnReloadCreatureAi.UseVisualStyleBackColor = True
-        '
-        'DataGridView4
-        '
-        Me.DataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView4.Location = New System.Drawing.Point(6, 35)
-        Me.DataGridView4.Name = "DataGridView4"
-        Me.DataGridView4.Size = New System.Drawing.Size(886, 364)
-        Me.DataGridView4.TabIndex = 5
+        Me.Button1.Location = New System.Drawing.Point(12, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(930, 472)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.tabMain)
         Me.Controls.Add(Me.btnQuit)
         Me.Name = "frmMain"
         Me.Text = "frmMain"
-        Me.tabMain.ResumeLayout(False)
-        Me.TabPage1.ResumeLayout(False)
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.TabPage2.ResumeLayout(False)
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.TabPage3.ResumeLayout(False)
-        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.TabPage4.ResumeLayout(False)
-        CType(Me.DataGridView4, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents btnQuit As System.Windows.Forms.Button
     Friend WithEvents tabMain As System.Windows.Forms.TabControl
-    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
-    Friend WithEvents btnReload As System.Windows.Forms.Button
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
-    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
-    Friend WithEvents btnReloadItems As System.Windows.Forms.Button
-    Friend WithEvents DataGridView2 As System.Windows.Forms.DataGridView
-    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
-    Friend WithEvents TabPage4 As System.Windows.Forms.TabPage
-    Friend WithEvents btnReloadGameObjects As System.Windows.Forms.Button
-    Friend WithEvents DataGridView3 As System.Windows.Forms.DataGridView
-    Friend WithEvents btnReloadCreatureAi As System.Windows.Forms.Button
-    Friend WithEvents DataGridView4 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
--- MaNGOSDatabaseTool/MaNGOSDatabaseTool/frmMain.vb
+++ MaNGOSDatabaseTool/MaNGOSDatabaseTool/frmMain.vb
@@ -5,49 +5,108 @@ Public Class frmMain
         End
     End Sub
 
-    Private Sub btnReload_Click(sender As Object, e As EventArgs) Handles btnReload.Click
-        Dim thisResult As New Framework.Database.SQLResult
-        Dim npcflag As New Dictionary(Of String, Int32)
-        npcflag.Add("npcflag", 0)
-        thisResult = DB.World.Select("SELECT * FROM creature_template WHERE npcflag<> @npcflag", npcflag)
+    'Private Sub btnReload_Click(sender As Object, e As EventArgs)
+    '    Dim thisResult As New Framework.Database.SQLResult
+    '    Dim npcflag As New Dictionary(Of String, Int32)
+    '    npcflag.Add("npcflag", 0)
+    '    thisResult = DB.World.Select("SELECT * FROM creature_template WHERE npcflag<> @npcflag", npcflag)
 
-        If thisResult.Count <> 0 Then
-            Me.DataGridView1.DataSource = thisResult
+    '    If thisResult.Count <> 0 Then
+    '        Me.DataGridView1.DataSource = thisResult
+    '    End If
+    'End Sub
+
+    'Private Sub btnReloadItems_Click(sender As Object, e As EventArgs)
+    '    Dim thisResult As New Framework.Database.SQLResult
+    '    Dim npcflag As New Dictionary(Of String, Int32)
+    '    npcflag.Add("npcflag", 0)
+    '    thisResult = DB.World.Select("SELECT Name, entry, class, subclass, displayid,quality FROM item_template ORDER BY Class, SubClass, Name")
+
+    '    If thisResult.Count <> 0 Then
+    '        Me.DataGridView2.DataSource = thisResult
+    '    End If
+
+    'End Sub
+
+    'Private Sub btnREloadGameObjects_Click(sender As Object, e As EventArgs)
+    '    Dim thisResult As New Framework.Database.SQLResult
+    '    Dim npcflag As New Dictionary(Of String, Int32)
+    '    npcflag.Add("npcflag", 0)
+    '    thisResult = DB.World.Select("SELECT NAME, faction, TYPE, entry, displayid,scriptname FROM Gameobject_template")
+
+    '    If thisResult.Count <> 0 Then
+    '        Me.DataGridView3.DataSource = thisResult
+    '    End If
+
+    'End Sub
+
+    'Private Sub btnReloadCreatureAi_Click(sender As Object, e As EventArgs)
+    '    Dim thisResult As New Framework.Database.SQLResult
+    '    Dim npcflag As New Dictionary(Of String, Int32)
+    '    npcflag.Add("npcflag", 0)
+    '    thisResult = DB.World.Select("SELECT * FROM creature_ai_scripts")
+
+    '    If thisResult.Count <> 0 Then
+    '        Me.DataGridView4.DataSource = thisResult
+    '    End If
+    'End Sub
+
+    Private Sub btnTabPage_Click(sender As Object, e As EventArgs)
+        Dim TableName As String = sender.name 'btnReloadaccount_access
+        TableName = TableName.Substring(9, TableName.Length - 9)
+        Dim thisResult As New Framework.Database.SQLResult
+        thisResult = DB.World.Select("SELECT * FROM " & TableName)
+        If Not IsNothing(thisResult) Then
+            If thisResult.Count <> 0 Then
+
+                Dim NewTab As TabPage
+                Dim Temp As DataGridView
+                NewTab = Me.tabMain.Controls.Item(TableName)
+                TableName = "DataGridView" & TableName
+                Temp = NewTab.Controls.Item(TableName)
+                Temp.DataSource = thisResult
+            End If
         End If
     End Sub
 
-    Private Sub btnReloadItems_Click(sender As Object, e As EventArgs) Handles btnReloadItems.Click
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
         Dim thisResult As New Framework.Database.SQLResult
         Dim npcflag As New Dictionary(Of String, Int32)
-        npcflag.Add("npcflag", 0)
-        thisResult = DB.World.Select("SELECT Name, entry, class, subclass, displayid,quality FROM item_template ORDER BY Class, SubClass, Name")
+        '        npcflag.Add("npcflag", 0)
+        thisResult = DB.World.Select("SELECT table_name FROM information_schema.tables WHERE table_type = 'base table' AND table_schema='test'")
 
         If thisResult.Count <> 0 Then
-            Me.DataGridView2.DataSource = thisResult
+            Dim tabName As String
+            For dbResults = 0 To thisResult.Count - 1
+                tabName = thisResult.Rows(dbResults).Item(0).ToString()
+                Me.tabMain.TabPages.Add(tabName, tabName)
+
+                '
+                'btnReload
+                '
+                Dim btnTemp As New System.Windows.Forms.Button()
+                btnTemp.Location = New System.Drawing.Point(6, 6)
+                btnTemp.Name = "btnReload" & tabName
+                btnTemp.Size = New System.Drawing.Size(75, 23)
+                btnTemp.TabIndex = 4
+                btnTemp.Text = "Reload"
+                btnTemp.UseVisualStyleBackColor = True
+                AddHandler btnTemp.Click, AddressOf Me.btnTabPage_Click
+                '
+                'DataGridView1
+                '
+                Dim DataGridViewTemp = New System.Windows.Forms.DataGridView()
+                DataGridViewTemp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+                DataGridViewTemp.Location = New System.Drawing.Point(6, 35)
+                DataGridViewTemp.Name = "DataGridView" & tabName
+                DataGridViewTemp.Size = New System.Drawing.Size(886, 364)
+                DataGridViewTemp.TabIndex = 3
+
+                Me.tabMain.TabPages(tabName).Controls.Add(btnTemp)
+                Me.tabMain.TabPages(tabName).Controls.Add(DataGridViewTemp)
+            Next
         End If
 
-    End Sub
-
-    Private Sub btnREloadGameObjects_Click(sender As Object, e As EventArgs) Handles btnReloadGameObjects.Click
-        Dim thisResult As New Framework.Database.SQLResult
-        Dim npcflag As New Dictionary(Of String, Int32)
-        npcflag.Add("npcflag", 0)
-        thisResult = DB.World.Select("SELECT NAME, faction, TYPE, entry, displayid,scriptname FROM Gameobject_template")
-
-        If thisResult.Count <> 0 Then
-            Me.DataGridView3.DataSource = thisResult
-        End If
-
-    End Sub
-
-    Private Sub btnReloadCreatureAi_Click(sender As Object, e As EventArgs) Handles btnReloadCreatureAi.Click
-        Dim thisResult As New Framework.Database.SQLResult
-        Dim npcflag As New Dictionary(Of String, Int32)
-        npcflag.Add("npcflag", 0)
-        thisResult = DB.World.Select("SELECT * FROM creature_ai_scripts")
-
-        If thisResult.Count <> 0 Then
-            Me.DataGridView4.DataSource = thisResult
-        End If
+        '        Me.tabMain.TabPages.Add("dddd" & Me.tabMain.TabPages.Count)
     End Sub
 End Class
