d66e07b1236f0cbfdad2ceddfef503940a895b2d
Salja <salja2012@hotmail.de>
2013-02-26 11:26:34 +0100

    [WoWLauncher] Not more needed Code
    Signed-off-by: Salja <salja2012@hotmail.de>

--- WoWLauncher/WoWLauncher/WoWLauncherMain.vb
+++ WoWLauncher/WoWLauncher/WoWLauncherMain.vb
@@ -1,27 +1,7 @@
 ﻿Public Class WoWLauncherMain
-    Dim IsDraggingForm As Boolean = False
-    Private MousePos As New System.Drawing.Point(0, 0)
-
-    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseDown
-        If e.Button = MouseButtons.Left Then
-            IsDraggingForm = True
-            MousePos = e.Location
-        End If
-    End Sub
-
-    Private Sub Form1_MouseUp(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseUp
-        If e.Button = MouseButtons.Left Then IsDraggingForm = False
-    End Sub
-
-    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles MyBase.MouseMove
-        If IsDraggingForm Then
-            Dim temp As Point = New Point(Me.Location + (e.Location - MousePos))
-            Me.Location = temp
-            temp = Nothing
-        End If
-    End Sub
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        WoWLauncherOptions.Visible = False
         PictureBox1.Parent = Me
         PictureBox2.Parent = Me
         PictureBox3.Parent = Me
@@ -30,7 +10,6 @@
         PictureBox2.BackColor = Color.Transparent
         PictureBox3.BackColor = Color.Transparent
         PictureBox4.BackColor = Color.Transparent
-        WoWLauncherOptions.Visible = False
     End Sub
 
     Private Sub PictureBox1_MouseEnter(sender As System.Object, e As System.EventArgs) Handles PictureBox1.MouseEnter
