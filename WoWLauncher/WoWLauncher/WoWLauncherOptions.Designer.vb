3cd3914fcfdb02fc361c19034d41c805763ea0ea
Salja <salja2012@hotmail.de>
2013-02-26 13:35:21 +0100

    [WoWLauncher] Change Realmlist Option
    Signed-off-by: Salja <salja2012@hotmail.de>

--- WoWLauncher/WoWLauncher/WoWLauncherOptions.Designer.vb
+++ WoWLauncher/WoWLauncher/WoWLauncherOptions.Designer.vb
@@ -25,6 +25,9 @@ Partial Class WoWLauncherOptions
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WoWLauncherOptions))
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
@@ -47,11 +50,42 @@ Partial Class WoWLauncherOptions
         Me.Button2.Text = "Cancel"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(87, 235)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(160, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = " logon.project-zero.eu"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(64, 212)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(107, 17)
+        Me.RadioButton1.TabIndex = 3
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Change Realmlist"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(172, 258)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 4
+        '
         'WoWLauncherOptions
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(340, 312)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -61,8 +95,12 @@ Partial Class WoWLauncherOptions
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Options"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
--- WoWLauncher/WoWLauncher/WoWLauncherOptions.vb
+++ WoWLauncher/WoWLauncher/WoWLauncherOptions.vb
@@ -3,4 +3,22 @@
     Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
         Me.Close()
     End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        If RadioButton1.Checked Then
+            Dim lines() As String = IO.File.ReadAllLines("realmlist.wtf")
+            lines(0) = "set realmlist" & TextBox1.Text
+            IO.File.WriteAllLines("realmlist.wtf", lines)
+            Label1.Text = "All Options Saved"
+        Else
+            Label1.Text = "Please select an option"
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles RadioButton1.CheckedChanged
+        If RadioButton1.Checked Then
+            TextBox1.Enabled = True
+            Label1.Text = Nothing
+        End If
+    End Sub
 End Class
